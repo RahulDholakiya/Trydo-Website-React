a0a1ffc674e06648407215201a65f5d74c41f9ad
rahul dholakiya <dholakiyarahul0@gmail.com>
2023-07-13 14:17:07 +0530

    startup

---
+++ trydo-website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HeaderPage from './Component/HeaderPage';
+
+function App() {
+  return (
+    <div>
+      <HeaderPage />
+    </div>
+  );
+}
+
+export default App;
